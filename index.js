3d25b8625f36c78c5ee509bdd49ded872da7c4f4
Ray Deck <ray@raydeck.com>
2019-05-31 07:05:59 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext } from "react";
+import {
+  ActionSheetProvider,
+  connectActionSheet
+} from "@expo/react-native-action-sheet";
+const context = createContext();
+const Provider = context.Provider;
+const ASHookProvider = connectActionSheet(
+  ({ children, showActionSheetWithOptions }) => {
+    <Provider value={showActionSheetWithOptions}>{children}</Provider>;
+  }
+);
+const TopProvider = props => (
+  <ActionSheetProvider>
+    <ASHookProvider {...props} />
+  </ActionSheetProvider>
+);
+const useShowActionSheetWithOptions = () => {
+  const showActionSheetWithOptions = useContext(context);
+  return showActionSheetWithOptions;
+};
+export {
+  TopProvider as ActionSheetProvider,
+  connectActionSheet,
+  useShowActionSheetWithOptions
+};
